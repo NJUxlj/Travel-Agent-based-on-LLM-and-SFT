998e4d5140038f2f9e80c94492a32104e7b529f0
zhongyah <1016509070@qq.com>
2024-05-11 23:25:31 +0800

    update

--- python_part/speech_to_text.py
+++ python_part/speech_to_text.py
@@ -7,8 +7,8 @@ import os
 
 # 代理地址
 # os.environ["OPENAI_API_BASE"] = "http://{your proxy url}/v1"
-os.environ['http_proxy'] = 'http://127.0.0.1:7890'
-os.environ['https_proxy'] = 'http://127.0.0.1:7890'
+# os.environ['http_proxy'] = 'http://127.0.0.1:7890'
+# os.environ['https_proxy'] = 'http://127.0.0.1:7890'
 
 
 
